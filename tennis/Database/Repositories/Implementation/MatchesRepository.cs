ee8b85021de516aa7ee17424c21f381cdcf5586f
Denis <denis09112004@gmail.com>
2024-04-08 16:42:04 +0300

    create MatchesRepository and interface

---
+++ tennis/Database/Repositories/Implementation/MatchesRepository.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using frontend.Repositories;
+using tennis_scoreboard.Models;
+
+namespace tennis.Database.Repositories.Implementation;
+
+public class MatchesRepository: IMatchesRepository
+{
+    public void Create(Player player1, Player player2, Player winner)
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            connection.Query<Match>("INSERT INTO [Matches] (Player1, Player2, Winner) VALUES (@p1, @p2, @w);", new { p1 = player1.Id, p2 = player2.Id, w = winner.Id });
+        }
+    }
+    
+    public List<Match> GetAll()
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            return connection.Query<Match>("SELECT * FROM [Matches];").ToList();
+        }
+    }
+    
+    public Match GetById(Guid id)
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            return connection.QueryFirstOrDefault<Match>("SELECT * FROM [Matches] WHERE Id = @i;", new { i = id });
+        }
+    }
+}
---
+++ tennis/Database/Repositories/Interfaces/IMatchesRepository.cs
@@ -0,0 +1,11 @@
+﻿using tennis_scoreboard.Models;
+
+namespace frontend.Repositories;
+
+public interface IMatchesRepository
+{
+    public void Create(Player player1, Player player2, Player winner);
+    public List<Match> GetAll();
+    public Match GetById(Guid id);
+
+}
