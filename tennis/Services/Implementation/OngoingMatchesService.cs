0de2e0653ed65781f612fdc075359f05268a1720
Denis <denis09112004@gmail.com>
2024-04-08 23:16:32 +0300

    created OngoingMatchesService (saving current matches in dictionary)

---
+++ tennis/Services/Implementation/OngoingMatchesService.cs
@@ -0,0 +1,33 @@
+﻿using tennis_scoreboard.Models;
+
+namespace tennis.Database.Services;
+
+public class OngoingMatchesService
+{
+    private Dictionary<Guid, NewMatch> currentMatches;
+
+    public OngoingMatchesService()
+    {
+        currentMatches = new Dictionary<Guid, NewMatch>();
+    }
+
+    public void AddMatch(NewMatch match)
+    {
+        currentMatches.Add(match.Id, match);
+    }
+
+    public NewMatch GetMatch(Guid uuid)
+    {
+        if (currentMatches.TryGetValue(uuid, out NewMatch match))
+        {
+            return match;
+        }
+
+        return null;
+    }
+
+    public void DeleteMatch(Guid uuid)
+    {
+        currentMatches.Remove(uuid);
+    }
+}
