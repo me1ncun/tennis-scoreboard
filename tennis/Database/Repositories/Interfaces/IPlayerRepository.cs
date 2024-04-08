4a507e7ec92d407db9b89a382328e1d53b003309
Denis <denis09112004@gmail.com>
2024-04-08 16:41:43 +0300

    create PlayerRepository and interface

--- tennis/Database/Repositories/Interfaces/IPlayerRepository.cs
+++ tennis/Database/Repositories/Interfaces/IPlayerRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace frontend.Repositories;
+﻿using tennis_scoreboard.Models;
+
+namespace frontend.Repositories;
 
 public interface IPlayerRepository
 {
-    
+    public void Register(string name);
+    public bool Exists(string name);
+    public List<Player> GetAll();
+    public Player GetByName(string name);
+
 }
