eed4c8cd1f809bf36b1af4c33fb6a7f02dc56b62
Denis <denis09112004@gmail.com>
2024-04-08 23:19:00 +0300

    divided services and repositories into folders

---
+++ tennis/Repositories/Implementation/PlayerRepository.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using frontend.Repositories;
+using tennis_scoreboard.Models;
+
+namespace tennis.Database.Repositories.Implementation;
+
+public class PlayerRepository : IPlayerRepository
+{
+    public void Register(string name) 
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            try
+            {
+                connection.Query("INSERT INTO [Players] (Name) VALUES (@n);", new { n = name });
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    
+    public bool Exists(string name)
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            return connection.QueryFirstOrDefault<Player>("SELECT * FROM [Players] WHERE Name = @n;", new { n = name }) != null;
+        }
+    }
+    
+    public List<Player> GetAll()
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            return connection.Query<Player>("SELECT * FROM [Players];").ToList();
+        }
+    }
+    
+    public Player GetByName(string name)
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            return connection.QueryFirstOrDefault<Player>("SELECT * FROM [Players] WHERE Name = @n;", new { n = name });
+        }
+    }
+}
