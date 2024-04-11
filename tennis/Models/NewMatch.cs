e03f1597cda27182b82bc8dc1b3544c4f8a63a46
Denis <denis09112004@gmail.com>
2024-04-11 22:53:59 +0300

    deleted not used props

--- tennis/Models/NewMatch.cs
+++ tennis/Models/NewMatch.cs
@@ -1,17 +1,12 @@
-﻿using tennis.Score.Score_system;
+﻿using tennis_scoreboard.DTO;
+using tennis.Score.Score_system;
 
 namespace tennis_scoreboard.Models
 {
     public class NewMatch
     {
         public Guid Id { get; set; }
-        public string Player1 { get; set; }
-        public string Player2 { get; set; }
-        public Point ScorePlayer1 { get; set; }
-        public Point ScorePlayer2 { get; set; }
-        public Set SetPlayer1 { get; set; } 
-        public Set SetPlayer2 { get; set; }
-        public Game GamePlayer1 { get; set; }
-        public Game GamePlayer2 { get; set; }
+        public PlayerDTO Player1 { get; set; }
+        public PlayerDTO Player2 { get; set; }
     }
 }
