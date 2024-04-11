2343d9f7fce76638142508e7b20bf9239721baac
Denis <denis09112004@gmail.com>
2024-04-11 22:53:34 +0300

    add CheckGameWinner, CheckSetWinner, CheckMatchWinner methods

--- tennis/Services/Implementation/MatchScoreCalculationService.cs
+++ tennis/Services/Implementation/MatchScoreCalculationService.cs
@@ -1,47 +1,95 @@
 ﻿
 using Newtonsoft.Json;
+using tennis_scoreboard.DTO;
 using tennis_scoreboard.Models;
+using tennis.Database.Repositories.Implementation;
 using tennis.Score.Score_system;
 
 namespace tennis.Database.Services;
 
 public class MatchScoreCalculationService
 {
+    private readonly MatchesRepository _matchesRepository;
     private readonly NewMatch _match;
     public MatchScoreCalculationService(NewMatch match)
     {
         _match = match;
+        _matchesRepository = new MatchesRepository();
     }
 
-    public void IncreasePlayerScore(Point player, int oppponentScore)
+    public void IncreasePlayerScore(PlayerDTO player, PlayerDTO oppponent)
     {
-        switch (player.amount)
+        switch (player.Scores.amount)
         {
             case 0:
-                player.IncreaseCounter(15);
+                player.Scores.IncreaseCounter(15);
                 break;
             case 15:
-                player.IncreaseCounter(15);
+                player.Scores.IncreaseCounter(15);
                 break;
             case 30:
-                player.IncreaseCounter(10);
+                player.Scores.IncreaseCounter(10);
                 break;
-            case 40: 
-                CheckWinner(player, oppponentScore);
+            case 40:
+                CheckMatchWinner(player, oppponent);
+                CheckSetWinner(player, oppponent);
+                CheckGameWinner(player, oppponent);
                 break;
-            default: player.IncreaseCounter(1);
+            default: player.Scores.IncreaseCounter(1);
                 break;
         }
     }
-    public void CheckWinner(Point playerPoint, int oppponentScore)
+    public void CheckGameWinner(PlayerDTO player, PlayerDTO oppponent)
     { 
-        if (playerPoint.amount == 40)
+        if(oppponent.Scores.amount == player.Scores.amount && player.Scores.amount == 40) 
         {
-            _match.GamePlayer1.IncreaseCounter();
-            playerPoint.amount = 0;
-            _match.ScorePlayer2.amount = 0;
+            player.Scores.amount = 0;
+            _match.Player2.Scores.amount = 0;
+            
+            player.Scores.IncreaseCounter(1);
+        }
+        else if (oppponent.Scores.amount == 40)
+        {
+            oppponent.Games.IncreaseCounter();
+            oppponent.Scores.amount = 0;
+            _match.Player1.Scores.amount = 0;
+        }
+        else if (player.Scores.amount == 40)
+        {
+            player.Games.IncreaseCounter();
+            player.Scores.amount = 0;
+            _match.Player2.Scores.amount = 0;
         }
     }
     
+    public void CheckSetWinner(PlayerDTO player, PlayerDTO oppponent)
+    {
+        if (player.Games.amount == 6 && oppponent.Games.amount < 5)
+        {
+            player.Sets.IncreaseCounter();
+            player.Games.amount = 0;
+            oppponent.Games.amount = 0;
+        }
+        else if (player.Games.amount == 7 && oppponent.Games.amount == 5)
+        {
+            player.Sets.IncreaseCounter();
+            player.Games.amount = 0;
+            oppponent.Games.amount = 0;
+        }
+    }
+    
+    public void CheckMatchWinner(PlayerDTO player, PlayerDTO oppponent)
+    {
+        if (player.Sets.amount == 2)
+        {
+            _matchesRepository.Create(player, oppponent, player);
+            _match.Player1 = player;
+        }
+        else if (oppponent.Sets.amount == 2)
+        {
+            _matchesRepository.Create(oppponent, player, oppponent);
+            _match.Player2 = oppponent;
+        }
+    }
     
 }
