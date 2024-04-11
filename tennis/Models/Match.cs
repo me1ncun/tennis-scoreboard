21c33331f6d5d8fd0474b2f57384d780e4f253bf
Denis <denis09112004@gmail.com>
2024-04-11 22:51:36 +0300

    add some properties to model

--- tennis/Models/Match.cs
+++ tennis/Models/Match.cs
@@ -2,8 +2,12 @@
 {
     public class Match
     {
-        public Player Player1 { get; set; }
-        public Player Player2 { get; set; }
-        public Player Winner { get; set; }
+        public int Player1 { get; set; }
+        public int Player2 { get; set; }
+        public int Winner { get; set; }
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+        public string WinnerName { get; set; }
+        
     }
 }
