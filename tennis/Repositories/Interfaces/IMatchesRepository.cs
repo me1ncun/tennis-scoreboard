e8954a1b5cd63e236ec990c7bec24ad7d0b73830
Denis <denis09112004@gmail.com>
2024-04-11 22:51:23 +0300

    change params in method

--- tennis/Repositories/Interfaces/IMatchesRepository.cs
+++ tennis/Repositories/Interfaces/IMatchesRepository.cs
@@ -1,11 +1,12 @@
-﻿using tennis_scoreboard.Models;
+﻿using tennis_scoreboard.DTO;
+using tennis_scoreboard.Models;
 
 namespace frontend.Repositories;
 
 public interface IMatchesRepository
 {
-    public void Create(Player player1, Player player2, Player winner);
-    public List<Match> GetAll();
+    public void Create(PlayerDTO player1, PlayerDTO player2, PlayerDTO winner);
+    public IEnumerable<Match> GetAll();
     public Match GetById(Guid id);
 
 }
