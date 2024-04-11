bb51bf069a30c2e6c339a70bdb67bf403f4f2022
Denis <denis09112004@gmail.com>
2024-04-11 22:52:10 +0300

    add GetNameById and GetIdByName

--- tennis/Repositories/Implementation/MatchesRepository.cs
+++ tennis/Repositories/Implementation/MatchesRepository.cs
@@ -1,24 +1,25 @@
 ﻿using Dapper;
 using frontend.Repositories;
+using tennis_scoreboard.DTO;
 using tennis_scoreboard.Models;
 
 namespace tennis.Database.Repositories.Implementation;
 
 public class MatchesRepository: IMatchesRepository
 {
-    public void Create(Player player1, Player player2, Player winner)
+    public void Create(PlayerDTO player1, PlayerDTO player2, PlayerDTO winner)
     {
         using (var connection = AppDbContext.CreateConnection())
         {
-            connection.Query<Match>("INSERT INTO [Matches] (Player1, Player2, Winner) VALUES (@p1, @p2, @w);", new { p1 = player1.Id, p2 = player2.Id, w = winner.Id });
+            connection.Query<Match>("INSERT INTO [Matches] (Player1, Player2, Winner) VALUES (@p1, @p2, @w);", new { p1 = GetIdByName(player1.Name), p2 = GetIdByName(player2.Name), w = GetIdByName(winner.Name) });
         }
     }
     
-    public List<Match> GetAll()
+    public IEnumerable<Match> GetAll()
     {
         using (var connection = AppDbContext.CreateConnection())
         {
-            return connection.Query<Match>("SELECT * FROM [Matches];").ToList();
+            return connection.Query<Match>("SELECT * FROM [Matches];").AsEnumerable();
         }
     }
     
@@ -27,6 +28,22 @@ public class MatchesRepository: IMatchesRepository
         using (var connection = AppDbContext.CreateConnection())
         {
             return connection.QueryFirstOrDefault<Match>("SELECT * FROM [Matches] WHERE Id = @i;", new { i = id });
+        }
+    }
+    
+    public string GetNameById(int id)
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            return connection.QueryFirstOrDefault<string>("SELECT Name FROM [Players] WHERE ID = @i;", new { i = id });
+        }
+    }
+    
+    public int GetIdByName(string name)
+    {
+        using (var connection = AppDbContext.CreateConnection())
+        {
+            return connection.QueryFirstOrDefault<int>("SELECT ID FROM [Players] WHERE Name = @n;", new { n = name });
         }
     }
 }
