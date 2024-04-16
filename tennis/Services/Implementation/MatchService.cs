41a8a8c7d4f248f101334b9afdd1df1740d66d69
Denis <denis09112004@gmail.com>
2024-04-16 21:29:20 +0300

    create methods

--- tennis/Services/Implementation/MatchService.cs
+++ tennis/Services/Implementation/MatchService.cs
@@ -1,4 +1,5 @@
-﻿using tennis_scoreboard.DTO;
+﻿using frontend.Repositories;
+using tennis_scoreboard.DTO;
 using tennis_scoreboard.Models;
 using tennis.Database.Repositories.Implementation;
 
@@ -6,24 +7,70 @@ namespace tennis.Database.Services;
 
 public class MatchService
 {
-    private readonly MatchesRepository _matchesRepository;
-    public MatchService(MatchesRepository matchesRepository)
+    private readonly IMatchesRepository _matchesRepository;
+    public MatchService(IMatchesRepository matchesRepository)
     {
         _matchesRepository = matchesRepository;
     }
     
     public void CreateMatch(PlayerDTO player1, PlayerDTO player2, PlayerDTO winner)
     {
-        _matchesRepository.Create(player1, player2, winner);
+        var player1Id = _matchesRepository.GetIdByName(player1.Name);
+        var player2Id = _matchesRepository.GetIdByName(player2.Name);
+        var winnerId = _matchesRepository.GetIdByName(winner.Name);
+        _matchesRepository.Create(player1Id, player2Id, winnerId);
     }
     
-    public IEnumerable<Match> GetAllMatches()
+    public List<Match> GetAllMatches()
     {
-        return _matchesRepository.GetAll();
+        var matches = _matchesRepository.GetAll();
+        // Add player names to matches
+        List<Match> matchesWithName = new List<Match>();
+        foreach (var match in matches)
+        {
+            Match matchChanged = new Match()
+            {
+                Player1Name = GetNameById(match.Player1),
+                Player2Name = GetNameById(match.Player2),
+                WinnerName = GetNameById(match.Winner)
+            };
+            matchesWithName.Add(matchChanged);
+        }
+
+        return matchesWithName;
     }
     
     public Match GetMatchById(Guid id)
     {
-        return _matchesRepository.GetById(id);
+        return _matchesRepository.GetMatchByGuid(id);
+    }
+    
+    public int GetIdByName(string name)
+    {
+        return _matchesRepository.GetIdByName(name);
+    }
+    
+    public string GetNameById(int id)
+    {
+        return _matchesRepository.GetNameById(id);
+    }
+    
+    public List<Match> GetMatchesByPlayerName(string name)
+    {
+        var matches =  _matchesRepository.GetMatchesByPlayerName(name);
+        // Add player names to matches
+        List<Match> matchesWithName = new List<Match>();
+        foreach (var match in matches)
+        {
+            Match matchChanged = new Match()
+            {
+                Player1Name = GetNameById(match.Player1),
+                Player2Name = GetNameById(match.Player2),
+                WinnerName = GetNameById(match.Winner)
+            };
+            matchesWithName.Add(matchChanged);
+        }
+
+        return matchesWithName;
     }
 }
