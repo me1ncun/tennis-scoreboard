fed4cab33ad3306e92d1a90d9d3221b93fdf1357
Denis <denis09112004@gmail.com>
2024-04-16 21:26:39 +0300

    add method to interface of the repository

--- tennis/Repositories/Interfaces/IMatchesRepository.cs
+++ tennis/Repositories/Interfaces/IMatchesRepository.cs
@@ -5,8 +5,11 @@ namespace frontend.Repositories;
 
 public interface IMatchesRepository
 {
-    public void Create(PlayerDTO player1, PlayerDTO player2, PlayerDTO winner);
-    public IEnumerable<Match> GetAll();
-    public Match GetById(Guid id);
+    public void Create(int player1Id, int player2Id, int winnerId);
+    public List<Match> GetAll();
+    public Match GetMatchByGuid(Guid id);
+    public string GetNameById(int id);
+    public int GetIdByName(string name);
+    public List<Match> GetMatchesByPlayerName(string name);
 
 }
