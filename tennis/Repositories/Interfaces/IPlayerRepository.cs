e62c9e6e32af97a88ba842d3bc534d8caa8f532c
Denis <denis09112004@gmail.com>
2024-04-17 22:03:02 +0300

    refactoring code naming method

--- tennis/Repositories/Interfaces/IPlayerRepository.cs
+++ tennis/Repositories/Interfaces/IPlayerRepository.cs
@@ -4,7 +4,7 @@ namespace frontend.Repositories;
 
 public interface IPlayerRepository
 {
-    public void Register(string name);
+    public void RegisterIfNotExist(string name);
     public List<Player> GetAllPlayers();
     public Player GetPlayerByName(string name);
 
--- tennis/Services/Interfaces/IPlayerService.cs
+++ tennis/Services/Interfaces/IPlayerService.cs
@@ -3,7 +3,7 @@ using tennis_scoreboard.Models;
 
 public interface IPlayerService
 {
-    public void Register(string name);
+    public void RegisterIfNotExist(string name);
     public List<Player> GetAll();
     public Player GetByName(string name);
     
