a7f9971f13aee47d5f8aee61e9289202d166ce18
Denis <denis09112004@gmail.com>
2024-04-18 13:47:51 +0300

    code refactoring to make the code more readable

--- tennis/Controllers/HomeController.cs
+++ tennis/Controllers/HomeController.cs
@@ -2,46 +2,25 @@
 using Newtonsoft.Json;
 using tennis_scoreboard.DTO;
 using tennis_scoreboard.Models;
-using tennis.Database.Repositories.Implementation;
 using tennis.Database.Services;
 using tennis.Score.Score_system;
+using tennis.Utils;
 
 namespace frontend.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IPlayerService _playerService;
-        private readonly MatchesRepository _matchesRepository;
-        public HomeController(IPlayerService playerService)
+        private readonly MatchService _matchService;
+        private readonly MatchesUtil _matchesUtil;
+        private readonly MatchScoreCalculationService _matchScoreCalculationService;
+
+        public HomeController(IPlayerService playerService, MatchService matchService, MatchesUtil matchesUtil)
         {
             _playerService = playerService;
-            _matchesRepository = new MatchesRepository();
-        }
-
-        [HttpGet("finished-matches")]
-        public IActionResult FinishedMatches()
-        {
-            var matches = _matchesRepository.GetAll();
-            
-            List<Match> matchesWithName = new List<Match>();
-            foreach (var match in matches)
-            {
-                Match matchChanged = new Match()
-                {
-                    Player1Name = _matchesRepository.GetNameById(match.Player1),
-                    Player2Name = _matchesRepository.GetNameById(match.Player2),
-                    WinnerName = _matchesRepository.GetNameById(match.Winner)
-                };
-                matchesWithName.Add(matchChanged);
-            }
-            if (matches != null)
-            {
-                return View(matchesWithName);
-            }
-            else
-            {
-                return NotFound();
-            }
+            _matchService = matchService;
+            _matchesUtil = matchesUtil;
+            _matchScoreCalculationService = new MatchScoreCalculationService();
         }
 
         [HttpGet]
@@ -49,13 +28,13 @@ namespace frontend.Controllers
         {
             return View();
         }
-        
+
         [HttpGet]
         public IActionResult Privacy()
         {
             return View();
         }
-        
+
         [HttpGet("new-match")]
         public IActionResult NewMatch()
         {
@@ -63,34 +42,19 @@ namespace frontend.Controllers
         }
 
         [HttpPost("new-match")]
-        public IActionResult NewMatch(MatchDTO matchDto)
+        public IActionResult NewMatch(Registration registration)
         {
-            _playerService.Register(matchDto.Player1Name);
-            _playerService.Register(matchDto.Player2Name);
+            // register players if they don't exist
+            _playerService.RegisterIfNotExist(registration.Player1Name);
+            _playerService.RegisterIfNotExist(registration.Player2Name);
 
-            var match = new NewMatch
-            {
-                Id = Guid.NewGuid(),
-                Player1 = new PlayerDTO()
-                {
-                    Name = matchDto.Player1Name,
-                    Scores = new Point(),
-                    Sets = new Set(),
-                    Games = new Game()
-                },
-                Player2 = new PlayerDTO()
-                {
-                    Name = matchDto.Player2Name,
-                    Scores = new Point(),
-                    Sets = new Set(),
-                    Games = new Game()
-                },
+            // create match object
+            var match = _matchesUtil.GetToScoreDTO(registration);
 
-            };
-            
+            // serialized match
             string serializedMatch = JsonConvert.SerializeObject(match);
-            
-            /*_ongoingMatchesService.AddMatch(match);*/
+
+            // save match and matchId to session
             HttpContext.Session.SetString("MatchId", match.Id.ToString());
             HttpContext.Session.SetString("Match", serializedMatch);
 
@@ -104,51 +68,102 @@ namespace frontend.Controllers
             {
                 return BadRequest("Invalid UUID");
             }
-            
-            NewMatch match = GetMatch();
 
-            if (match == null)
+            MatchScoreDTO matchScoreDto = GetCurrentMatch();
+
+            if (matchScoreDto == null)
             {
                 return NotFound("Match not found");
             }
 
-            return View(match);
+            return View(matchScoreDto);
         }
-        
+
         [HttpPost("match-score")]
-        public IActionResult MatchScore(string player, string opponent)
+        public IActionResult MatchScore(string selectedPlayer, [FromQuery] Guid uuid)
         {
-            NewMatch match = GetMatch();
-            MatchScoreCalculationService _matchScoreCalculationService = new MatchScoreCalculationService(match);
-            if (player == "Player1")
+            MatchScoreDTO matchScoreDto = GetCurrentMatch();
+
+            // Add a point to the winner of the game
+            if (selectedPlayer == "Player1")
             {
-                _matchScoreCalculationService.IncreasePlayerScore(match.Player1, match.Player2);
+                _matchScoreCalculationService.AddPointToWinnerOfGame(matchScoreDto.Player1, matchScoreDto);
             }
-            else if(player == "Player2")
+            else if (selectedPlayer == "Player2")
             {
-                _matchScoreCalculationService.IncreasePlayerScore(match.Player2, match.Player1);
+                _matchScoreCalculationService.AddPointToWinnerOfGame(matchScoreDto.Player2, matchScoreDto);
             }
-            
-            if (match.Player1.Sets.amount == 2 || match.Player2.Sets.amount == 2)
+
+            // check if match is finished
+            if (matchScoreDto.Player1.Sets.amount == 2 || matchScoreDto.Player2.Sets.amount == 2)
             {
-                // Redirect to another page indicating match result
+                // set winner
+                PlayerScoreDTO winnerScoreDto = matchScoreDto.Player1.Sets.amount == 2
+                    ? matchScoreDto.Player1
+                    : matchScoreDto.Player2;
+
+                // create match in db
+                _matchService.CreateMatch(winnerScoreDto,
+                    winnerScoreDto == matchScoreDto.Player1 ? matchScoreDto.Player2 : matchScoreDto.Player1,
+                    winnerScoreDto);
+
+                // render final score
                 return RedirectToAction("FinishedMatches", "Home");
             }
-            
-            // save changes to session
-            string serializedMatch = JsonConvert.SerializeObject(match);
+
+            // Save changes to session
+            string serializedMatch = JsonConvert.SerializeObject(matchScoreDto);
             HttpContext.Session.SetString("Match", serializedMatch);
-            
-            return View(match);
+
+            // get guid match from session
+            var guid = HttpContext.Session.GetString(matchScoreDto.Id.ToString());
+            return RedirectToAction("MatchScore", "Home", new { uuid = matchScoreDto.Id.ToString() });
         }
-        
-        
-        // additional methods:
-        public NewMatch GetMatch()
+
+        [HttpGet("finished-matches")]
+        public IActionResult FinishedMatches(int page = 1, string filter_by_player_name = "")
+        {
+            const int PageSize = 5;
+            int totalMatchesCount = _matchService.GetAllMatches().Count;
+            int totalPages = (totalMatchesCount + PageSize - 1) / PageSize;
+
+            ViewData["TotalPages"] = totalPages;
+            ViewData["CurrentPage"] = page;
+            ViewData["PlayerName"] = filter_by_player_name;
+
+            List<Match> matches;
+            matches = _matchService.GetAllMatches().Skip((page - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+
+            return View(matches);
+        }
+
+        [HttpPost("finished-matches")]
+        public IActionResult FinishedMatches(string searchedPlayer, int page = 1, string filter_by_player_name = "")
+        {
+            const int PageSize = 5;
+            int totalMatchesCount = _matchService.GetMatchesByPlayerName(searchedPlayer).Count;
+            int totalPages = (totalMatchesCount + PageSize - 1) / PageSize;
+
+            ViewData["TotalPages"] = totalPages;
+            ViewData["CurrentPage"] = page;
+            ViewData["PlayerName"] = searchedPlayer;
+
+            List<Match> matches;
+            matches = _matchService.GetMatchesByPlayerName(searchedPlayer).Skip((page - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+
+            return View(matches);
+        }
+
+        // additional method
+        public MatchScoreDTO GetCurrentMatch()
         {
             string serializedMatchFromSession = HttpContext.Session.GetString("Match");
-            NewMatch match = JsonConvert.DeserializeObject<NewMatch>(serializedMatchFromSession);
-            return match;
+            MatchScoreDTO matchScoreDto = JsonConvert.DeserializeObject<MatchScoreDTO>(serializedMatchFromSession);
+            return matchScoreDto;
         }
     }
 }
