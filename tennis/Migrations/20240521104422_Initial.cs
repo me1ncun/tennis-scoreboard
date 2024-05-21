93b5ff7c5ac84ff7f8d4947d22af90673222fd07
Denis <denis09112004@gmail.com>
2024-05-21 14:05:51 +0300

    database migrations ef core

---
+++ tennis/Migrations/20240521104422_Initial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace tennis.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "matches",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    player1 = table.Column<int>(type: "integer", nullable: false),
+                    player2 = table.Column<int>(type: "integer", nullable: false),
+                    winner = table.Column<int>(type: "integer", nullable: false),
+                    Player1Name = table.Column<string>(type: "text", nullable: false),
+                    Player2Name = table.Column<string>(type: "text", nullable: false),
+                    WinnerName = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_matches", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "players",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_players", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "matches");
+
+            migrationBuilder.DropTable(
+                name: "players");
+        }
+    }
+}
