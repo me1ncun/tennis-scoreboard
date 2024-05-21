de257a3a45d6a27e4c5e8a367ee2ae062dd9db89
Denis <denis09112004@gmail.com>
2024-05-21 14:02:21 +0300

    created MigrationExtension

---
+++ tennis/Extensions/MigrationExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace tennis.Extensions;
+
+public static class MigrationExtension
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        using AppDbContext dbContext =
+            scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        dbContext.Database.Migrate();
+    }
+}
